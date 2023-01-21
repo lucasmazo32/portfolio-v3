a02701f360e541168891635318b3a3e9357faf78
Lucas <lucas@stanza.co>
2023-01-21 10:16:41 -0500

    Deploy hero image

--- src/features/Hero/HeroImage.tsx
+++ src/features/Hero/HeroImage.tsx
@@ -3,7 +3,7 @@ import Image from '../../assets/illustration.png'
 
 export const HeroImage: FC = () => {
   return (
-    <div className="lg:absolute 2xl:left-[800px] xl:left-[640px] lg:left-[624px] left-[640px] xl:top-36 lg:top-44 top-36 z-0">
+    <div className="lg:absolute 2xl:left-[1000px] xl:left-[760px] lg:left-[640px] left-[640px] xl:top-36 lg:top-44 top-36 z-0">
       <img
         className="xl:h-[494px] lg:h-[380px] md:h-[320px] h-[280px] ring-1 ring-base-200 ring-offset-4 ring-offset-base-100 rounded-full mx-auto"
         src={Image}
--- src/features/Hero/HeroText.tsx
+++ src/features/Hero/HeroText.tsx
@@ -3,9 +3,9 @@ import { FC } from 'react'
 export const HeroText: FC = () => {
   return (
     <p className="2xl:text-3xl md:text-2xl text-lg font-light relative z-10 lg:text-start text-center">
-      I help individuals and organizations transform product ideas&nbsp;
+      I help individuals and organizations transform product&nbsp;
       <br className="hidden lg:block" />
-      and designs into amazing user experiences
+      ideas and designs into amazing user experiences
     </p>
   )
 }
